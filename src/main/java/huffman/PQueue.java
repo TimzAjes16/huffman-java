1270228cf02f06419a07c3b217a4f8bd463bde4c
Timi Ajeigbe <timiajeigbe@gmail.com>
2022-08-03 01:12:41 +0100

    First iteration of Task 1 of enqueue, with other methods working

--- src/main/java/huffman/PQueue.java
+++ src/main/java/huffman/PQueue.java
@@ -1,8 +1,10 @@
 package huffman;
 
+import huffman.tree.Leaf;
 import huffman.tree.Node;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 /**
  * A priority queue of @Node@ objects. Each node has an int as its label representing its frequency.
@@ -10,10 +12,11 @@ import java.util.List;
  */
 public class PQueue {
 
-    private List<Node> queue;
+    private List<Node> queue; // can access this 'queue' attribute with 'this' 
+    private int itemsInList = 0;
 
     public PQueue() {
-        queue = new ArrayList<>();
+        queue = new ArrayList<>(); // each time constructor is called a new queue will be created
     }
 
     /**
@@ -22,7 +25,53 @@ public class PQueue {
      * @param n The node to enqueue.
      */
     public void enqueue(Node n) {
-        throw new UnsupportedOperationException("Method not implemented");
+        // throw new UnsupportedOperationException("Method not implemented");
+        /*
+         * 1. We need to check if queue is full or at least equal to inital capacity [x]
+         * 2. We need to add element to queue in constructtor [x]
+         * 3. We want to remove this element from the queue [x]
+         * 4. Adding to private data type queue and order setting the pirority varaible [x]
+         */
+        
+        int initialCapicity = 10;
+        boolean addedToArray = false;
+        
+        if (queue.size() == initialCapicity){
+            // Then the queue is full, and we cannot add an item.
+            System.out.println("Queue is full.");
+
+        } else {
+            int sizeofQueue = queue.size();
+            if (sizeofQueue == 0){
+                queue.add(n);
+            } else{
+                for(int i=0; i < sizeofQueue; i++){
+                    char LabelOfItem = ((Leaf) queue.get(i)).getLabel(); // casting from 'Node' data type to 'Leaf'
+                    
+                    System.out.println(n); 
+                    if ((((Leaf) n).getLabel()) >= LabelOfItem && addedToArray == false){
+                        Node removedItem = (Leaf) queue.remove(i);
+                        queue.add(0,n); // adding item to beginning of list.
+                        queue.add(i+1, removedItem); // added +1 to swap the index posiitons of previously removed list item.
+                        addedToArray = true;
+                    } else if ((((Leaf) n).getLabel()) >= LabelOfItem && addedToArray == true && i >= 1){
+                        // Node removedItem = (Leaf) queue.remove(i);
+                        // queue.add(i,removedItem);
+                        continue;
+                    } else if ((((Leaf) n).getLabel()) >= LabelOfItem && addedToArray == true){
+                        continue;
+                    }
+
+                }
+            }
+            
+            
+        }
+
+        System.out.println(String.format("%s", queue, "\n")); // form of variable testing so I can see the logic on-screen
+        System.out.println(queue.get(0).getClass().getName());
+
+
     }
 
     /**
@@ -30,7 +79,34 @@ public class PQueue {
      * @return  The first node in the queue.
      */
     public Node dequeue() {
-        throw new UnsupportedOperationException("Method not implemented");
+        // throw new UnsupportedOperationException("Method not implemented");
+
+        /*
+         * Just neeed to implement dequeue which should in theory return the first node of the list.
+         * But first I need to check if queue is empty.
+         */
+        
+        if (queue.size() == 0){
+            System.out.println("Queue is empty, cannot delete item.");
+            return null;
+        } else {
+            Node deletedNode = queue.remove(0);
+            return deletedNode;
+            
+        }
+        
+        /*
+         * was having errors with the above code due to the fact that if I used a single if block the compiler would 
+         * keep telling me to 'return Node', however I realised this was due to the fact that I wasn't returning a value
+         * in the first if statement. (I was only printing)
+         * 
+         * Read further that this could be solved using a return statement in an if-else block of code.
+         */
+    
+        
+
+    
+
     }
 
     /**
@@ -38,6 +114,7 @@ public class PQueue {
      * @return  Size of the queue.
      */
     public int size() {
-        throw new UnsupportedOperationException("Method not implemented");
+        // throw new UnsupportedOperationException("Method not implemented");
+        return queue.size();
     }
 }
